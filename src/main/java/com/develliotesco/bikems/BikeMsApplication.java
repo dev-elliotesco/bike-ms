351ac890cd3628e504418856f6b238dd301d672c
Elliot Escovitch <dev.escovitch.ri@gmail.com>
2024-04-20 16:03:06 -0500

    add README.md, DB configuration and swagger documentation

--- src/main/java/com/develliotesco/bikems/BikeMsApplication.java
+++ src/main/java/com/develliotesco/bikems/BikeMsApplication.java
@@ -1,5 +1,7 @@
 package com.develliotesco.bikems;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +9,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BikeMsApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(BikeMsApplication.class, args);
+		final Logger log = LoggerFactory.getLogger(BikeMsApplication.class);
+		log.info("Bienvenido a demo de microservicios - bike ms");
 	}
 
 }

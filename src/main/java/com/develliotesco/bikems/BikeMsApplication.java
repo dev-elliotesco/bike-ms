f097d937e7fc00633aa3840017b8da16fea6be74
Elliot Escovitch <dev.escovitch.ri@gmail.com>
2024-04-20 12:50:19 -0500

    first commit

---
+++ src/main/java/com/develliotesco/bikems/BikeMsApplication.java
@@ -0,0 +1,13 @@
+package com.develliotesco.bikems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BikeMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BikeMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/develliotesco/bikems/BikeMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.develliotesco.bikems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BikeMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
